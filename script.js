ae96ee7841bac5283248129bc5c9af9993e8d578
C4llmeAnthony <a.lebiocki@onet.pl>
2026-09-22 19:55:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-// Lista piosenek z oficjalnymi adresami URL do plików audio (previews)
+// Dokładna lista Twoich plików mp3 (z uwzględnieniem spacji i wielkich liter)
 const bratSongs = [
-  { title: "360", url: "https://kfs.io" },
-  { title: "Club classics", url: "https://kfs.io" },
-  { title: "Sympathy is a knife", url: "https://kfs.io" },
-  { title: "I might say something stupid", url: "https://kfs.io" },
-  { title: "Talk talk", url: "https://kfs.io" },
-  { title: "Von dutch", url: "https://kfs.io" },
-  { title: "Everything is romantic", url: "https://kfs.io" },
-  { title: "Rewind", url: "https://kfs.io" },
-  { title: "So I", url: "https://kfs.io" },
-  { title: "Girl, so confusing", url: "https://kfs.io" },
-  { title: "Apple", url: "https://kfs.io" },
-  { title: "B2b", url: "https://kfs.io" },
-  { title: "365", url: "https://kfs.io" }
+  { title: "360", file: "360.mp3" },
+  { title: "365", file: "365.mp3" },
+  { title: "apple", file: "Apple.mp3" },
+  { title: "b2b", file: "B2b.mp3" },
+  { title: "club classics", file: "Club classics.mp3" },
+  { title: "everything is romantic", file: "Everything is romantic.mp3" },
+  { title: "girl, so confusing", file: "Girl, so confusing.mp3" },
+  { title: "i might say something stupid", file: "I might say something stupid.mp3" },
+  { title: "i think about it all the time", file: "I think about it all the time.mp3" },
+  { title: "mean girls", file: "Mean girls.mp3" },
+  { title: "rewind", file: "Rewind.mp3" },
+  { title: "so i", file: "So I.mp3" },
+  { title: "sympathy is a knife", file: "Sympathy is a knife.mp3" },
+  { title: "talk talk", file: "Talk talk.mp3" },
+  { title: "von dutch", file: "Von dutch.mp3" }
 ];
 
 const button = document.getElementById('bratButton');
@@ -24,7 +26,7 @@ let lastSelectedIndex = -1;
 button.addEventListener('click', () => {
   let randomIndex;
   
-  // Losowanie (zabezpieczenie przed powtórkami pod rząd)
+  // Zabezpieczenie przed wylosowaniem tej samej piosenki dwa razy z rzędu
   do {
     randomIndex = Math.floor(Math.random() * bratSongs.length);
   } while (randomIndex === lastSelectedIndex);
@@ -32,12 +34,14 @@ button.addEventListener('click', () => {
   lastSelectedIndex = randomIndex;
   const selectedSong = bratSongs[randomIndex];
 
-  // Zmiana tekstu i źródła dźwięku
+  // Wyświetlenie tytułu (zgodnie ze stylem płyty, małymi literami)
   message.textContent = selectedSong.title;
-  audioPlayer.src = selectedSong.url;
   
-  // Wymuszenie odtworzenia po kliknięciu
+  // Wskazanie pliku mp3 znajdującego się w tym samym folderze
+  audioPlayer.src = selectedSong.file;
+  
+  // Odtwarzanie dźwięku
   audioPlayer.play().catch(error => {
-    console.log("Odtwarzanie zablokowane przez przeglądarkę:", error);
+    console.log("Przeglądarka zablokowała odtwarzanie: ", error);
   });
 });
